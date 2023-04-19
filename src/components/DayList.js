3fd22ae8ba491f3913aa5658708296f4701a90c2
Ramic <ramlag1819@gmail.com>
2023-04-18 21:41:13 -0700

    Create DayList component and test it with Storybook by using map to render all day objects

---
+++ src/components/DayList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DayListItem from "./DayListItem";
+
+const DayList = (props) => {
+  const days = props.days.map(day => <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={() => props.setDay(day.name)} />)
+  return (
+    <ul>
+      {days}
+    </ul>
+  )
+}
+
+export default DayList
