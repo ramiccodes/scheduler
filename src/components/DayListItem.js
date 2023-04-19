d73bf0cc87c887260046f60c0c96fc9a0f84925e
Ramic <ramlag1819@gmail.com>
2023-04-18 20:08:40 -0700

    Add DayListItem component, styling, and testing with Storybook and Jest with conditional rendering

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
+
+export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  })
+
+  // const formatSpots = () => {
+  //   if (props.spots === 0) {
+  //     return <h3 className="text--light">no spots remaining</h3>
+  //   } else if (props.spots === 1) {
+  //     return <h3 className="text--light">1 spot remaining</h3>
+  //   }
+  //   return <h3 className="text--light">{props.spots} spots remaining</h3>
+  // }
+
+  return (
+    <li className={dayClass} onClick={() => {props.setDay(props.name)}}>
+      <h2 className="text--regular">{props.name}</h2>
+      {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+      {props.spots === 1 && <h3 className="text--light">{props.spots} spot remaining</h3>}
+      {props.spots > 1 && <h3 className="text--light">{props.spots} spots remaining</h3>}
+    </li>
+  );
+}
