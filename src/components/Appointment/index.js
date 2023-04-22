fd91f70b6f79ce6fc623300ac06d3937ba619f82
Ramic <ramlag1819@gmail.com>
2023-04-21 19:45:26 -0700

    Create custom hook named useVisualMode to render out different components based on the current and past mode of the add interview component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,13 +2,35 @@ import React from "react";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import "components/Appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 const Appointment = (props) => {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE, false)} />}
+      {mode === SHOW && (
+        <Show 
+          student={props.interview.student} 
+          interviewer={props.interview.interviewer.name} 
+        />
+      )} 
+      {mode === CREATE && (
+        <Form 
+          interviewers={[]}
+          onSave={() => console.log("Somebody you loved")}
+          onCancel={() => back()}
+        />
+      )}
     </article>
   )
 }
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const useVisualMode = (visualMode) => {
+  const [mode, setMode] = useState(visualMode);
+  const [history, setHistory] = useState([visualMode])
+
+  const transition = (newMode, replace = false) => {
+    if (replace) {
+      setMode(newMode);
+      setHistory((prev) => [...prev]);
+    } else {
+      setMode(newMode);
+      setHistory(oldHistory => [...oldHistory, newMode]);
+    }
+  }
+
+  const back = () => {
+    if (history.length === 1) {
+      return
+    }
+    if (history.length !== 0) {
+      history.pop();
+      setMode(history[history.length -1])
+    }
+  }
+
+  return {mode, transition, back};
+}
+
+export default useVisualMode;
